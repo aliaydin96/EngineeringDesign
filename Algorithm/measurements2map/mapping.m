8f63ff9956b2291023b613c5a2f79e217c351865
DESKTOP-6IJJOUF\nailt <nailtosun07@gmail.com>
2018-12-13 01:16:48 +0300

    Multiple measurement to map algorithm implemented.

---
+++ Algorithm/measurements2map/mapping.m
@@ -0,0 +1,17 @@
+    classdef mapping
+       properties
+          Angle
+          Position
+          Measurement
+       end
+       methods
+%           function data = Rotate(obj, obj)
+%              data = rotate([obj.Angle],data);
+%           end
+%           function data = flipflop(obj)
+%              data = obj.Measurement;
+%              data = data(1,end:-1:1);
+%           end
+       end
+    end
+    % mapping.Angle = 10, mapping.Position = [ 1 2 3 5]; mapping.Measurement = [x,x,x];
