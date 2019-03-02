56d0da4952d62a007b5d2b0d0d7779aef26863be
Enes <enes.ayaz.1996@gmail.com>
2019-03-02 16:22:17 +0300

    Meas

---
+++ Algorithm/measurements2map/csvwriting.m
@@ -0,0 +1,37 @@
+%%
+Angle_of_Measurement_1 = meas_1.Angle;
+Self_Localization_Measurement_1= meas_1.Position;
+Measurement_1= meas_1.Measurement;
+
+csvwrite('C:\Users\enes_\Desktop\Mapping\Coordinate_Meas_1', Measurement_1);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Self_Localization_Meas_1', Self_Localization_Measurement_1);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Heading_Meas_1', Angle_of_Measurement_1);
+
+%%
+Angle_of_Measurement_2 = meas_2.Angle;
+Self_Localization_Measurement_2= meas_2.Position;
+Measurement_2= meas_2.Measurement;
+
+csvwrite('C:\Users\enes_\Desktop\Mapping\Coordinate_Meas_2', Measurement_2);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Self_Localization_Meas_2', Self_Localization_Measurement_2);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Heading_Meas_2', Angle_of_Measurement_2);
+
+%%
+Angle_of_Measurement_3 = meas_3.Angle;
+Self_Localization_Measurement_3= meas_3.Position;
+Measurement_3= meas_3.Measurement;
+
+csvwrite('C:\Users\enes_\Desktop\Mapping\Coordinate_Meas_3', Measurement_3);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Self_Localization_Meas_3', Self_Localization_Measurement_3);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Heading_Meas_3', Angle_of_Measurement_3);
+
+%%
+Angle_of_Measurement_4 = meas_4.Angle;
+Self_Localization_Measurement_4= meas_4.Position;
+Measurement_4= meas_4.Measurement;
+
+csvwrite('C:\Users\enes_\Desktop\Mapping\Coordinate_Meas_4', Measurement_4);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Self_Localization_Meas_4', Self_Localization_Measurement_4);
+csvwrite('C:\Users\enes_\Desktop\Mapping\Heading_Meas_4', Angle_of_Measurement_4);
+
+
